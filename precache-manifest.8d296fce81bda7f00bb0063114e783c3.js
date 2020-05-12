12fcec9e11eee5a322792ff3331cc770d0d0f128
tom2kota <tom2kota@gmail.com>
2020-05-12 23:04:13 +0000

    Updates

---
+++ precache-manifest.8d296fce81bda7f00bb0063114e783c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee0cdb3e0cf0445b3243a582e48af92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f3e9ad856306e0c7695f",
+    "url": "./static/js/2.fc2bcc4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fc2bcc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950df5ee8be5400830f8",
+    "url": "./static/js/main.9e57b4a7.chunk.js"
+  },
+  {
+    "revision": "5d3d2cfe34ec4b1d23c8",
+    "url": "./static/js/runtime-main.28843803.js"
+  }
+]);
